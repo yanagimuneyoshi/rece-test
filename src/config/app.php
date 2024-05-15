48b614c1d381a474796834e34fca34a7637f4e13
yanagimuneyoshi <yanagimuneyoshi1994gmail.com>
2024-05-16 01:39:53 +0900

    2024

--- src/config/app.php
+++ src/config/app.php
@@ -80,7 +80,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ja',
 
     /*
     |--------------------------------------------------------------------------
@@ -140,6 +140,7 @@ return [
          * Laravel Framework Service Providers...
          */
         Illuminate\Auth\AuthServiceProvider::class,
+        App\Providers\FortifyServiceProvider::class,
         Illuminate\Broadcasting\BroadcastServiceProvider::class,
         Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
